9556f9c3b8c59c029f3df75872764e9e73e8b467
Artur Mustafin <artur.mustafin@gmail.com>
2014-02-06 14:12:09 +0400

    added project
    added initial commit for project for challenge #1

---
+++ Challenges/1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Challenges
+{
+    internal interface IChallenge
+    {
+        void Main(string[] args);
+    }
+
+    internal class Challenge : IChallenge
+    {
+        public void Main(string[] args)
+        {
+        }
+    }
+
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            return Run<Challenge>(args);
+        }
+
+        private static int Run<T>(string[] args) where T : IChallenge, new()
+        {
+            try
+            {
+                if (args == null)
+                {
+                    throw new Exception("args == null");
+                }
+                if (args.Length == 0)
+                {
+                    throw new Exception("args.Length == 0");
+                }
+                if (args.Length > 1)
+                {
+                    throw new Exception("args.Length > 1");
+                }
+                if (!File.Exists(args[0]))
+                {
+                    throw new Exception("!File.Exists(args[0])");
+                }
+                var t = new T();
+                t.Main(args);
+            }
+            catch (Exception ex)
+            {
+                var exception = ex;
+                while (exception != null)
+                {
+                    Console.Error.WriteLine(ex.Message);
+                    Console.Error.WriteLine(ex.StackTrace);
+                    exception = ex.InnerException;
+                }
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
