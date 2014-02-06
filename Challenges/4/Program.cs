388d1af29cd93520caba06fb1414dad830cd6bf1
Artur Mustafin <artur.mustafin@gmail.com>
2014-02-06 09:09:11 +0400

    final
    evaluate sum of first 1000 primes

--- Challenges/4/Program.cs
+++ Challenges/4/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System.Linq;
-using NUnit.Framework;
 
 #region
 
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
 
 #endregion
 
@@ -15,101 +12,49 @@ using System.Text;
 namespace Challenges
 // ReSharper restore CheckNamespace
 {
-    internal interface IChallenge
+    class Challenge
     {
-        void Main(string[] args);
-    }
-
-    internal class Challenge : IChallenge
-    {
-        public void Main(string[] args)
+        static void Main()
         {
             List<int> primes = new List<int>();
-            List<int> primes_sqrt = new List<int>();
             primes.Add(2);
             primes.Add(3);
+            int primesSqrt = 1;
             int prime = 5;
             while (primes.Count < 1000)
             {
-                bool isprime = true;
-                bool isprimeplus2 = true;
-                foreach (var i in primes_sqrt)
+                bool isprime1 = true;
+                bool isprime2 = true;
+                for (int i = 0; i < primes.Count && primes[i] <= primesSqrt; i++)
                 {
-                    if (isprime && (prime%i) == 0)
+                    if (isprime1 && (prime % primes[i]) == 0)
                     {
-                        isprime = false;
+                        isprime1 = false;
                     }
-                    if (isprimeplus2 && ((prime + 2)%i) == 0)
+                    if (isprime2 && ((prime + 2) % primes[i]) == 0)
                     {
-                        isprimeplus2 = false;
+                        isprime2 = false;
                     }
-                    if (!isprime && !isprimeplus2)
+                    if (!isprime1 && !isprime2)
                     {
                         break;
                     }
                 }
-                if (isprime)
+                if (isprime1)
                 {
                     primes.Add(prime);
                 }
-                if (isprimeplus2)
+                if (isprime2)
                 {
                     primes.Add(prime + 2);
                 }
-                if (isprime || isprimeplus2)
+                if (isprime1 || isprime2)
                 {
-                    int sqrt = (int) Math.Floor(Math.Sqrt(prime));
-                    if (primes_sqrt[primes_sqrt.Count - 1] != sqrt)
-                        primes_sqrt.Add(sqrt);
+                    primesSqrt = (int)Math.Ceiling(Math.Sqrt(prime + 2));
                 }
                 prime += 6;
             }
             Console.WriteLine(primes.Sum());
-        }
-    }
-
-    internal class Program
-    {
-        private static int Main(string[] args)
-        {
-            return Run<Challenge>(args);
-        }
-
-        private static int Run<T>(string[] args) where T : IChallenge, new()
-        {
-            try
-            {
-                if (args == null)
-                {
-                    throw new Exception("args == null");
-                }
-                if (args.Length == 0)
-                {
-                    throw new Exception("args.Length == 0");
-                }
-                if (args.Length > 1)
-                {
-                    throw new Exception("args.Length > 1");
-                }
-                if (!File.Exists(args[0]))
-                {
-                    throw new Exception("!File.Exists(args[0])");
-                }
-                var t = new T();
-                t.Main(args);
-            }
-            catch (Exception ex)
-            {
-                var exception = ex;
-                while (exception != null)
-                {
-                    Console.Error.WriteLine(ex.Message);
-                    Console.Error.WriteLine(ex.StackTrace);
-                    exception = ex.InnerException;
-                }
-                return -1;
-            }
-            return 0;
         }
     }
 }
